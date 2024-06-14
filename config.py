43aef2271fcd184e0c7bef68a9ae48d23580f5a7
Devil999-tevhno <akshbusy@gmail.com>
2024-06-14 16:49:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,17 +22,17 @@ API_HASH = os.environ.get("8517235398f1f2fe1dcee4a71b29a4b7", "")
 CHANNEL_ID = int(os.environ.get("-1002242740472", ""))
 
 #OWNER ID
-OWNER_ID = 6686798585", "6589551297"))
+OWNER_ID = int(os.environ.get("6686798585", "6589551297"))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("NarutoProductions", "narutoProductions")
+DB_URI = os.environ.get("mongodb+srv://akshbusy:akshansh11@cluster0.cpm1nww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", "")
+DB_NAME = os.environ.get("Naruto Productions", "Naruto Productions")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("-1002186342338", ""))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -61,7 +61,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+ADMINS.append(6589551297, 6686798585)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
